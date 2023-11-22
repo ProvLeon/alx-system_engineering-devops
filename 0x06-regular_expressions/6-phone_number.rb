751e5a4a95bb2c8e0554256fdc7955b9eef1d8aa
ProvLeon <providence.leonard@gmail.com>
2023-11-22 17:36:26 +0000

    updated

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,2 +1,17 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^\d{10,10}$/).join
+
+def valid_phone_number?(string)
+  regex = /\A\d{10}\z/
+  string.match?(regex)
+end
+
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <phone_number>"
+else
+  input_number = ARGV[0]
+  if valid_phone_number?(input_number)
+    puts input_number
+  else
+    puts "Invalid phone number"
+  end
+end
