d828838e71d137d832c72c83856c7410d9430ef7
ProvLeon <providence.leonard@gmail.com>
2023-11-22 16:41:03 +0000

    Nov 22, 2023, 4:41 PM

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,12 @@
 #!/usr/bin/env ruby
 puts ARGV[0].scan(/School/).join
+
+# A function that prints a greeting message with the given name.
+#
+# Args:
+#     name (str): The name to include in the greeting message.
+#
+# Returns:
+#     None: This function does not return anything.
+def hello(name):
+  print("hello " + name)
