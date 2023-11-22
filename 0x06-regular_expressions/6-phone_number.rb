1e5d7991d29440e8cd19bd34cae320c540420600
ProvLeon <providence.leonard@gmail.com>
2023-11-22 17:44:45 +0000

    Nov 22, 2023, 5:44 PM

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,19 +1,2 @@
 #!/usr/bin/env ruby
-
-def extract_phone_number(string)
-  regex = /\d{10}/
-  match_data = string.match(regex)
-  match_data ? match_data[0] : ""
-end
-
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <phone_number>"
-else
-  input_number = ARGV[0]
-  extracted_number = extract_phone_number(input_number)
-  if extracted_number.length == 10
-    puts extracted_number
-  else
-    puts "Invalid phone number"
-  end
-end
+puts ARGV[0].scan(/^\d{10}$/).join
