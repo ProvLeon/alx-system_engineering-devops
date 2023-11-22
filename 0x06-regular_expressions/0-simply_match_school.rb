16dff593a0934bd37876abd7b7bbb62cc87f494d
ProvLeon <providence.leonard@gmail.com>
2023-11-22 17:15:26 +0000

    Nov 22, 2023, 5:15 PM

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(string)
-  regex = /School/
-  match_data = string.match(regex)
-  match_data ? match_data[0] : ""
-end
-
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-else
-  input_string = ARGV[0]
-  matched = match_school(input_string)
-  puts matched
-end
+puts ARGV[0].scan(/School/).join
--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_hbttn(string)
-  regex = /hb(t{2,5})n/
-  match_data = string.match(regex)
-  match_data ? match_data[0] : ""
-end
-
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-else
-  input_string = ARGV[0]
-  matched = match_hbttn(input_string)
-  puts matched
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
