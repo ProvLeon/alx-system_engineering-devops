d552f32fe0eeb8b810a0b04d2c35eb2013196eee
ProvLeon <providence.leonard@gmail.com>
2024-01-26 07:21:34 +0000

    completed and updated task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # kills a program ('killmenow') with the pkill command
 
 exec { 'killmenow_process':
-  command     => 'pkill killmenow',
-  onlyif      => 'pgrep killmenow',
-  path        => '/usr/bin',
+  command => 'pkill killmenow',
+  onlyif  => 'pgrep killmenow',
+  path    => '/usr/bin',
 }
