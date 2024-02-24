54a128a74689a7189e0ec26a3d63b32391fef3e1
ProvLeon <providence.leonard@gmail.com>
2024-02-24 11:05:05 +0000

    completed

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,43 +1,20 @@
-# A puppet manifest to install Nginx
-class nginx_install {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    require => Package['nginx'],
-  }
-
-  file { '/usr/share/nginx/html/index.html':
-    ensure  => file,
-    content => 'Hello World!',
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => '
-server {
-  listen 80 default_server;
-  listen [::]:80 default_server;
-  root /usr/share/nginx/html;
-  index index.html;
-  server_name _;
-
-  location / {
-    try_files $uri $uri/ =404;
-  }
-
-  location /redirect_me {
-    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-  }
-}',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
+# Install Nginx with puppet
+package { 'nginx':
+  ensure => installed,
 }
 
-include nginx_install
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;',
+}
 
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
