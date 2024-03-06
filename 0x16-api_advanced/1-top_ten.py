70bf8376a6bf123d7544e9fb1c75e0ce5e63fcac
ProvLeon <providence.leonard@gmail.com>
2024-03-06 05:51:35 +0000

    fixed coding style errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,12 +7,17 @@ import requests
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit."""
+    """
+    Queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
+    """
     headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'limit': 10}
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url,
+                            headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code != 200:
         print(None)
     else:
