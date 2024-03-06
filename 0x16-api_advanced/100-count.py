42fa101c207986b363b54d21d5fa150f87a49f45
ProvLeon <providence.leonard@gmail.com>
2024-03-06 05:48:05 +0000

    done

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -61,11 +61,11 @@ def count_words(subreddit, word_list):
 
     recurse(subreddit, dictionary)
 
-    l = sorted(dictionary.items(), key=lambda kv: kv[1])
-    l.reverse()
+    List = sorted(dictionary.items(), key=lambda kv: kv[1])
+    List.reverse()
 
-    if len(l) != 0:
-        for item in l:
+    if len(List) != 0:
+        for item in List:
             if item[1] is not 0:
                 print("{}: {}".format(item[0], item[1]))
     else:
